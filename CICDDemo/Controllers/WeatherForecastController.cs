e84781aab6cc0278d25130f0597b5265ef529ea5
Danish <danishelahi016@gmail.com>
2025-10-19 13:26:59 +0500

    Get endpoint

--- CICDDemo/Controllers/WeatherForecastController.cs
+++ CICDDemo/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace CICDDemo.Controllers
             _logger = logger;
         }
 
-        [HttpPost(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
